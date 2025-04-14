3a6f9483dc435d7896d04aa3b807f730c8fd0981
bccalegari <brunocostacallegari@gmail.com>
2025-04-14 20:43:19 -0300

    refactor: dev compose, devfiles external network
    plus version txt on banner

--- src/app/enterprise/infra/config/banner.py
+++ src/app/enterprise/infra/config/banner.py
@@ -1,7 +1,13 @@
 def show_banner():
+    try :
+        with open('version.txt', 'r') as file:
+            version = file.read().strip()
+    except Exception as e :
+        version = "Error reading version file"
+
     with open('banner.txt', 'r') as file:
         banner = file.read()
         banner = banner.replace("{project_name}", "dev-files-ai")
-        banner = banner.replace("{version}", "0.1.0")
+        banner = banner.replace("{version}", version)
 
     print("\n" + banner + "\n")
